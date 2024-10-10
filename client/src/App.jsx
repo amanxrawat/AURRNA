f8e28463d68a0894116ce90c16b41c4817990112
amanxrawat <amanxrawat.93@gmail.com>
2024-10-10 21:01:18 +0530

    resolved merge conflicts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,39 +3,54 @@ import { useEffect, lazy, Suspense } from "react";
 
 import Loader from "./components/layout/Loader";
 import ProtectedRoutes from "./components/auth/ProtectedRoutes";
+import axios from "axios";
+import { server } from "./constants/config";
+import { useDispatch, useSelector } from "react-redux";
 
-const Home = lazy(() => import("./pages/Home"))
-const Login = lazy(() => import("./pages/Login"))
-const Profile = lazy(() => import("./pages/Profile"))
-const Search = lazy(() => import("./pages/Search"))
-const Favourite = lazy(() => import("./pages/Favourite"))
-const Cart = lazy(() => import("./pages/Cart"))
-const Contact = lazy(() => import("./pages/Contact"))
-const About = lazy(() => import("./pages/About"))
+import { userNotExists } from "./redux/features/auth/authSlice";
 
-const user = false;
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Profile = lazy(() => import("./pages/Profile"));
+const Search = lazy(() => import("./pages/Search"));
+const Favourite = lazy(() => import("./pages/Favourite"));
+const Cart = lazy(() => import("./pages/Cart"));
+const Contact = lazy(() => import("./pages/Contact"));
+const About = lazy(() => import("./pages/About"));
+const Product = lazy(() => import("./pages/Product"));
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/search/:filter?" element={<Search />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
+	const { user } = useSelector((state) => state.auth);
 
-          <Route element={<ProtectedRoutes />}>
-            <Route path="/favourite" element={<Favourite />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
+	const dispatch = useDispatch();
 
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
-  )
+	useEffect(() => {
+		axios
+			.get(`${server}/api/v1/user/login`)
+			.then((res) => console.log(res))
+			.catch((err) => dispatch(userNotExists()));
+	}, [dispatch]);
+
+	return (
+		<BrowserRouter>
+			<Suspense fallback={<Loader />}>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/search/:filter?" element={<Search />} />
+					<Route path="/cart" element={<Cart />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/product/:productId" element={<Product />} />
+
+					<Route element={<ProtectedRoutes />}>
+						<Route path="/favourite" element={<Favourite />} />
+						<Route path="/profile" element={<Profile />} />
+					</Route>
+				</Routes>
+			</Suspense>
+		</BrowserRouter>
+	);
 };
 
 export default App;
